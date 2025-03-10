a998044245fdebd8b3462f5da436790d58653262
Waldemar <41773009+integral96@users.noreply.github.com>
2025-03-10 11:49:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,13 +16,13 @@ int main()
     assert(matrix.size() == 1);
     // выведется одна строка
     // 100100314
-    // for(auto c: matrix) {
-    //     int x;
-    //     int y;
-    //     int v;
-    //     std::tie(x, y, v) = c;
-    //     std::cout << x << y << v << std::endl;
-    // }
+    for(auto c: matrix) {
+        int x;
+        int y;
+        int v;
+        std::tie(x, y, v) = c;
+        std::cout << x << y << v << std::endl;
+    }
     size_t cols = 10;
     size_t rows = 10;
     Matrix<int, -1, 2> matrix1(cols, rows);
@@ -33,11 +33,23 @@ int main()
     std::cout << "\nmatrix2D = \n" << matrix1 << std::endl;
     size_t N1 = 3;
     size_t N2 = 3;
-    size_t N3 = 4;
-    size_t N4 = 5;
-    Matrix<int, -1, 4> matrix4(N1, N2, N3, N4);
+    size_t N3 = 3;
+    size_t N4 = 3;
+    Matrix<int, 0, 4> matrix4(N1, N2, N3, N4);
+    for (size_t k = 0; k < N1; ++k)
+    for (size_t i = 0; i < N1; ++i)
+    for (size_t j = 0; j < N1; ++j) {
+       matrix4[k][i][j][j] = j + 1;
+       matrix4[k][i][j][(N1 - 1) - j] = (N1 - 1) - j + 1;
+    }
     std::cout << "\nmatrix4D =\n" << matrix4 << std::endl;
 
-    std::cout << "HELLO" << std::endl;
+    size_t N5 = 3;
+    size_t N6 = 3;
+    size_t N7 = 3;
+
+    Matrix<int, -1, 7> matrix7(N1, N2, N3, N4, N5, N6, N7);
+    std::cout << "\nmatrix7D =\n" << matrix7 << std::endl;
+
     return 0;
 }
